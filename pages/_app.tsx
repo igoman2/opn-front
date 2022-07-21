2fe55e12055f6a09d512ca6cfe07891a9da171d5
han.junho <han.junho@yanolja.com>
2022-07-22 01:01:26 +0900

    feat: meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,31 @@
 import type { AppProps } from "next/app";
 import { GlobalStyle } from "../styles/global-style";
+import Head from "next/head";
 import Layout from "../layouts/Layout";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../styles/theme";
 
 function App({ Component, pageProps }: AppProps) {
     return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </ThemeProvider>
+        <>
+            <Head>
+                <title>개원여기</title>
+                <meta
+                    name="description"
+                    content="서울부터 제주도까지, 각 의과별로 개원을 위한 정보를 찾아보세요."
+                />
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ThemeProvider>
+        </>
     );
 }
 
