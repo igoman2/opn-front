9c36ec9b131de2e7c88af824ae39feef2ead1604
han.junho <han.junho@yanolja.com>
2022-07-22 01:14:26 +0900

    refactor: next-seo 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,14 @@ import type { AppProps } from "next/app";
 import { GlobalStyle } from "../styles/global-style";
 import Head from "next/head";
 import Layout from "../layouts/Layout";
+import { NextSeo } from "next-seo";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../styles/theme";
 
 function App({ Component, pageProps }: AppProps) {
     return (
         <>
-            <Head>
+            {/* <Head>
                 <title>개원여기</title>
                 <meta
                     name="description"
@@ -18,7 +19,26 @@ function App({ Component, pageProps }: AppProps) {
                     name="viewport"
                     content="initial-scale=1.0, width=device-width"
                 />
-            </Head>
+            </Head> */}
+            <NextSeo
+                title="개원여기"
+                description="서울부터 제주도까지, 각 의과별로 개원을 위한 정보를 찾아보세요."
+                canonical="https://hospital-here.vercel.app/"
+                openGraph={{
+                    type: "website",
+                    url: "https://hospital-here.vercel.app/",
+                    title: "개원여기",
+                    description:
+                        "서울부터 제주도까지, 각 의과별로 개원을 위한 정보를 찾아보세요.",
+                    images: [
+                        {
+                            url: "https://opndoctor.com/images/logo-all-white-large-low-quality.png",
+                        },
+                    ],
+                    site_name: "hospital-here",
+                }}
+            />
+            ;
             <ThemeProvider theme={theme}>
                 <GlobalStyle />
                 <Layout>
